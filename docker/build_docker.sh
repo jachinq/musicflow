c29f9ec74102c931dd034b4032c46c9962738e4d
Jachin <1106025235@qq.com>
2025-01-03 19:06:48 +0800

    feat: build docker image

--- docker/build_docker.sh
+++ docker/build_docker.sh
@@ -6,24 +6,27 @@ mkdir build/data
 
 # start processing the database. read metadata from music files and store it in the database.
 
-cd ./metadata
+cd metadata
 pnpm install
 pnpm dev
 pnpm db_album
+cd ..
 
 # start building the backend
-cd ../server
+cd server
 cargo build --release --target x86_64-unknown-linux-musl
-cp target/x86_64-unknown-linux-musl/release/musicflow ../build/
+cp target/x86_64-unknown-linux-musl/release/musicflow .
 cp conf/config.json ../build/conf
+cd ..
 
 # start building the frontend
-cd ../web
+cd web
 pnpm install
 pnpm build
-cp -r dist/* ../build/web/
+cp -r dist/ ../docker/web/
+cd ..
 
 # build the docker image
-cd ../docker
+cd docker
 sudo docker build -t musicflow:latest .
 cp docker-compose.yml ../build/
