2fedc479fff864f7f5159330a0e4f3badaeb6bea
Jachin <JachinQ@163.com>
2026-01-08 10:00:33 +0800

    feat: pwa server worker

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -3,6 +3,18 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './lib/log'
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('/service-worker.js')
+      .then(registration => {
+        console.log('Service Worker 注册成功:', registration);
+      })
+      .catch(error => {
+        console.log('Service Worker 注册失败:', error);
+      });
+  });
+}
 
 const rootEl = document.getElementById('root');
 if (rootEl) {
